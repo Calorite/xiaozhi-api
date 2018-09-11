0995c67523d04d89d9f750dc29388acab38438ec
Calorite <yidi909@gmail.com>
2018-09-11 17:47:35 +0900

    2018/09/11

--- src/main/java/com/yidi/application/Application.java
+++ src/main/java/com/yidi/application/Application.java
@@ -6,11 +6,15 @@ import java.sql.SQLException;
 import com.yidi.service.MainService;
 
 public class Application {
+	private static String sender;
+	private String tousr;
+	private static String text;
+
 	public static void main(String[] args) throws UnsupportedEncodingException {
 		try {
-			String name=new String(args[0].getBytes(),"UTF-8");
-			String text=new String(args[2].getBytes(),"UTF-8");
-			MainService main=new MainService(name, "",text );
+			//String name=new String(args[0].getBytes(),"UTF-8");
+			//String text=new String(args[2].getBytes(),"UTF-8");
+			MainService main=new MainService(sender, "",text );
 			System.out.println(main.getReply());
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -18,4 +22,41 @@ public class Application {
 			System.out.println("false!");
 		}
 	}
+	/**
+	 * @return the sender
+	 */
+	public String getSender() {
+		return sender;
+	}
+	/**
+	 * @param sender the sender to set
+	 */
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	/**
+	 * @return the tousr
+	 */
+	public String getTousr() {
+		return tousr;
+	}
+	/**
+	 * @param tousr the tousr to set
+	 */
+	public void setTousr(String tousr) {
+		this.tousr = tousr;
+	}
+	/**
+	 * @return the text
+	 */
+	public String getText() {
+		return text;
+	}
+	/**
+	 * @param text the text to set
+	 */
+	public void setText(String text) {
+		this.text = text;
+	}
+
 }
